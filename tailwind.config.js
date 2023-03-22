d5fdb52e2c8df93a15fbdee389535518bb9f6ee1
cnem255 <caydennemec@icloud.com>
2023-03-21 20:36:50 -0400

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,32 @@ module.exports = {
   theme: {
     extend: {},
   },
+
+  daisyui: {
+    themes: [
+      {
+        cnem: {
+          primary: "#0284c7",
+
+          secondary: "#e11d48",
+
+          accent: "#9333ea",
+
+          neutral: "#292D32",
+
+          "base-100": "#344151",
+
+          info: "#7dd3fc",
+
+          success: "#22c55e",
+
+          warning: "#eab308",
+
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
+
   plugins: [require("daisyui")],
-}
+};
